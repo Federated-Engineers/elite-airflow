4f8ea6c442f61d7ddaf6690ce2b740148cdcc690
mayowa akinyele <mayowaakinyele@mac.speedport.ip>
2026-01-24 17:21:43 +0100

    fix lint

--- business_logic/test.py
+++ business_logic/test.py
@@ -1,9 +1,10 @@
-# from cryptography.fernet import Fernet
+import boto3
+from cryptography.fernet import Fernet
 
-# fernet_key = Fernet.generate_key()
-# print(fernet_key.decode()) 
-# import boto3
+fernet_key = Fernet.generate_key()
+print(fernet_key.decode())
 
-# s3 = boto3.resource('s3')
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
+
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
