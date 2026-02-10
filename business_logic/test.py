3b74befa45e761d5cb1eae1759bd43dfc29b1d5c
mayowa akinyele <mayowaakinyele@mayowas-MacBook-Pro.local>
2026-02-10 22:12:38 +0100

    adding example dag

--- business_logic/test.py
+++ business_logic/test.py
@@ -1,10 +1,1 @@
-import boto3
-from cryptography.fernet import Fernet
 
-fernet_key = Fernet.generate_key()
-print(fernet_key.decode())
-
-
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
