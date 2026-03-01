90fbe7ba7768f5ecf47d479158585992fc27af62
Basifrank <obasichigoziefrancis@gmail.com>
2026-03-01 18:09:45 -0500

    updating codes

--- dags/nordic_ingestion.py
+++ dags/nordic_ingestion.py
@@ -1,37 +1,47 @@
-from datetime import datetime, timedelta
+import datetime
+from datetime import timedelta
 
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from business_logic.nordic_ws.config.sheets import SHEETS
-from business_logic.nordic_ws.src.auth2 import get_gspread_client
-from business_logic.nordic_ws.src.ingest import ingest_sheet
+from airflow.operators.python import PythonOperator
+
+from nordic_utils.to_finance_s3 import write_to_finance_s3
+from nordic_utils.to_marketing_s3 import write_to_marketing_s3
+from nordic_utils.to_supply_chain_s3 import write_to_supply_chain_s3
+from nordic_utils.to_user_growth_s3 import write_to_user_growth_s3
 
 default_args = {
-    "owner": "elite-data-engineers",
-    "retries": 3,
-    "retry_delay": timedelta(minutes=5),
-    "email_on_failure": False,
-    "email": ["your-team@email.com"],
+    'start_date': datetime.datetime(2026, 1, 1),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=5),
+    'catchup': False
 }
 
-with DAG(
-    dag_id="nordic_peaks_ingestion",
+
+dag = DAG(
+    dag_id='nordic_firm_dag',
     default_args=default_args,
-    description="Daily ingestion from Google Sheets to S3",
-    schedule="0 6 * * *",   # runs every day at 6am UTC
-    start_date=datetime(2026, 1, 1),
-    catchup=False,
-    tags=["nordic_peaks", "ingestion", "google_sheets"],
-) as dag:
+    schedule='@daily',
+    description='A DAG to send google sheets data to S3',
+)
 
-    def run_ingest(sheet_config, **kwargs):
-        client = get_gspread_client()
-        ingest_sheet(client, sheet_config)
-
-    # Dynamically create a task per sheet
-    for sheet in SHEETS:
-        PythonOperator(
-            task_id=f"ingest_{sheet['name']}",
-            python_callable=run_ingest,
-            op_kwargs={"sheet_config": sheet},
-        )
+write_market_s3 = PythonOperator(
+        dag=dag,
+        python_callable=write_to_marketing_s3,
+        task_id='write_marketing_s3'
+    )
+write_finance_s3 = PythonOperator(
+        dag=dag,
+        python_callable=write_to_finance_s3,
+        task_id='write_finance_s3'
+    )
+write_supply_chain_s3 = PythonOperator(
+        dag=dag,
+        python_callable=write_to_supply_chain_s3,
+        task_id='write_supply_chain_s3'
+    )
+write_growth_s3 = PythonOperator(
+        dag=dag,
+        python_callable=write_to_user_growth_s3,
+        task_id='write_user_growth_s3'
+    )
+write_market_s3 >> write_finance_s3 >> write_supply_chain_s3 >> write_growth_s3
---
+++ dags/nordic_utils/to_marketing_s3.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from airflow.sdk import Variable
+
+import awswrangler as wr
+import boto3
+
+from .get_all_sheet_record import read_google_sheet
+
+
+def write_to_marketing_s3():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    raw_s3_bucket = "federated-engineers-staging-elite-data-lake"
+    raw_path_dir = "marketing_campaign_data"
+    path = f"s3://{raw_s3_bucket}/{raw_path_dir}/{now}"
+    
+    wr.engine.set("python")
+    
+    boto3.setup_default_session(
+        aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
+        region_name="eu-central-1"
+    )
+    
+    wr.s3.to_parquet(
+        df=read_google_sheet("13ULiowSb4JuNIaLldstlNB7sqr1WBe0Yrjm8ALEyLQ0"),
+        path=path,
+        dataset=True,
+        mode="append"
+    )
