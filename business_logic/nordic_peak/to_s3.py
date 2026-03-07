3a6db20a3ab008c2e3f4bec9edde0007215bf547
Basifrank <obasichigoziefrancis@gmail.com>
2026-03-07 12:14:52 -0500

    updated codes to meet production standards

--- business_logic/nordic_peak/to_s3.py
+++ business_logic/nordic_peak/to_s3.py
@@ -1,19 +1,8 @@
 from datetime import datetime
 
 import awswrangler as wr
-import boto3
-from airflow.sdk import Variable
 
 from .google_auth import read_google_sheet
-
-
-def setup_aws_session() -> None:
-    """Setup default AWS session with credentials from Airflow variables."""
-    boto3.setup_default_session(
-        aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
-        region_name="eu-central-1"
-    )
 
 
 def write_to_s3(sheet_id: str, path_dir: str) -> None:
@@ -25,7 +14,6 @@ def write_to_s3(sheet_id: str, path_dir: str) -> None:
         'marketing_campaign_data')
     """
     wr.engine.set("python")
-    setup_aws_session()
     now = datetime.now().strftime("%Y-%m-%d")
     raw_s3_bucket = "federated-engineers-production-elite-nordic-peak"
     path = f"s3://{raw_s3_bucket}/{path_dir}"
--- plugins/aws.py
+++ plugins/aws.py
@@ -1,5 +1,4 @@
 import boto3
-from airflow.sdk import Variable
 
 
 def get_ssm_parameter(ssm_parameter_name: str):
@@ -12,8 +11,6 @@ def get_ssm_parameter(ssm_parameter_name: str):
     ssm_parameter = ssm_parameter_name
     client = boto3.client(
                 'ssm',
-                aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
                 region_name='eu-central-1',
         )
     response = client.get_parameter(Name=ssm_parameter, WithDecryption=True)
