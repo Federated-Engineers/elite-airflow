970cf874d131e56b6bed80d00c27a15d72e0f181
mayowa akinyele <mayowaakinyele@mayowas-MacBook-Pro.local>
2026-03-08 10:35:48 +0100

    test

--- business_logic/live_demo.py
+++ business_logic/live_demo.py
@@ -6,4 +6,4 @@ def demo():
     wr.s3.to_parquet(
         df=pd.DataFrame({'col': [1, 2, 100]}),
         path='s3://poc-bucket-oremeta/prefix/my_file.parquet')
-    return "Data written to s3."
+    return "Data written to s3.."
