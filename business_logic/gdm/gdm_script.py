9b5b10603ca9827efd66f366f3de1944c6c11f79
Jhoie <victoriousjvictor@gmail.com>
2026-03-11 11:19:45 +0100

    Airflow DAG to fetch latest file

--- business_logic/gdm/gdm_script.py
+++ business_logic/gdm/gdm_script.py
@@ -1,24 +1,29 @@
 import awswrangler as wr
+import boto3
 
 bucket = "gdm-raw-data"
 folder = "daily_extracts"
-
 s3_path = f"s3://{bucket}/{folder}"
 
 
-def extract_portugal_data(s3_path):
-    files = wr.s3.list_objects(s3_path)
+def extract_portugal_data():
+
+    s3 = boto3.client("s3")
+    response = s3.list_objects_v2(
+        Bucket=bucket,
+        Prefix=f"{folder}/")
+
+    files = response.get("Contents", [])
+
     if len(files) == 0:
         raise ValueError(f"No file found in {s3_path}")
-    
-    metadata = wr.s3.describe_objects(files)
-    latest_file = max(metadata, key=lambda x: metadata[x]['LastModified'])
 
-    df = wr.s3.read_parquet(f"s3://{latest_file}")
+    latest_object = max(files, key=lambda x: x["LastModified"])
+    latest_file = f"s3://{bucket}/{latest_object['Key']}"
+    print(f"Latest file: {latest_file}")
+
+    df = wr.s3.read_parquet(latest_file)
     df_portugal = df[df["plant_country"] == "Portugal"]
+    print("Portugal data extracted successfully.")
 
-    # df_portugal.to_csv("portugal_data.csv", index=False)
-
-    return(df_portugal)
-
-extract_portugal_data(s3_path)
+    return df_portugal
