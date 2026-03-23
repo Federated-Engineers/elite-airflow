69d482d1fb0198cca37564d18359f7462792a3b2
Musili Adebayo <69276797+Musili-Adebayo@users.noreply.github.com>
2026-03-23 12:44:59 +0100

    Revert "added datetime and remove execution date"

---
+++ business_logic/gdm/s3_to_gsheets_extract.py
@@ -0,0 +1,59 @@
+import logging
+
+import awswrangler as wr
+from airflow.sdk import Variable
+
+from plugins.google_sheet import append_dataframe_to_sheet
+from plugins.s3_helper import get_latest_s3_file
+
+logger = logging.getLogger(__name__)
+
+BUCKET = "gdm-raw-data"
+FOLDER = "daily_extracts"
+SSM_PATH = "/production/google-service-account/credentials"
+
+
+def extract_portugal_data(spreadsheet_id, worksheet_name):
+    """Extracts Portugal data from the latest parquet file in S3 and appends
+    it to a Google Sheet. If a backfill file is specified in Airflow Variables,
+    it processes that file instead.
+
+    Args:
+        spreadsheet_id: The ID of the Google Sheet to append data to.
+        worksheet_name: The name of the worksheet within the Google Sheet
+            to append data to.
+    Returns:
+        Appends Portugal data to Google Sheets.
+    """
+
+    logger.info("Starting Portugal data extraction pipeline")
+
+    backfilled_object = Variable.get("backfill_file", default=None)
+
+    if backfilled_object:
+        logger.info(f"Backfill detected. Processing file: {backfilled_object}")
+        file_path = f"s3://{backfilled_object}"
+
+        df = wr.s3.read_parquet(file_path)
+        df_portugal = df[df["plant_country"] == "Portugal"]
+        logger.info(f"{len(df_portugal)} Backfill Portugal rows extracted")
+
+        Variable.delete("backfill_file")
+        logger.info("Backfill file processed and variable cleared")
+
+    else:
+        latest_file = get_latest_s3_file(BUCKET, FOLDER)
+        df = wr.s3.read_parquet(latest_file)
+        logger.info("Reading recent file completed")
+
+        df_portugal = df[df["plant_country"] == "Portugal"]
+        logger.info(f"{len(df_portugal)} Portugal data extracted successfully")
+
+    if df_portugal.empty:
+        logger.warning("No Portugal data found in the file.")
+
+    logger.info("Writing Portugal data to Google Sheets")
+
+    append_dataframe_to_sheet(df_portugal, spreadsheet_id, SSM_PATH,
+                              worksheet_name)
+    logger.info("Portugal data successfully written to Google Sheets")
---
+++ dags/gdm_s3_to_gsheets.py
@@ -0,0 +1,41 @@
+import logging
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.standard.operators.python import PythonOperator
+
+from business_logic.gdm.s3_to_gsheets_extract import extract_portugal_data
+
+logger = logging.getLogger(__name__)
+
+spreadsheet_id = Variable.get("portugal_spreadsheet_id")
+worksheet_name = Variable.get("portugal_worksheet_name")
+
+# spreadsheet_id = "1EJZr940oGXcAVr0KS5nMhmh7VzdYixojkrwquYpZyXw"
+# worksheet_name = "Sheet1"
+
+default_args = {
+    "owner": "gdm",
+    "start_date": datetime(2026, 1, 1),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="gdm_s3_to_gsheets",
+    default_args=default_args,
+    schedule="0 8 * * *",
+    description="Extract Portugal data from S3 and push to Google Sheets",
+    catchup=False,
+    tags=["gdm", "s3", "google_sheets"],
+) as dag:
+
+    extract_and_push_to_sheet = PythonOperator(
+        task_id="extract_and_push_to_sheet",
+        python_callable=extract_portugal_data,
+        op_kwargs={
+            "spreadsheet_id": spreadsheet_id,
+            "worksheet_name": worksheet_name,
+        },
+    )
