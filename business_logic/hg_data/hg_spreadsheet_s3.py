5b44c10d7b0be470cda2a06e8d5f77cfa9f0f423
Chizobaeze <chizobaeze15@gmail.com>
2026-04-05 18:21:32 +0100

    Refactor S3 write function to include logging and remove duplicates; update DAG configuration with start date and tags

--- business_logic/hg_data/hg_spreadsheet_s3.py
+++ business_logic/hg_data/hg_spreadsheet_s3.py
@@ -9,7 +9,7 @@ from plugins.google_sheet import get_data_from_gsheet
 
 logger = logging.getLogger(__name__)
 
-# Load configuration
+
 config = Variable.get("hg_config", deserialize_json=True)
 BUCKET_NAME = config["bucket_name"]
 FOLDER_NAME = config["folder_name"]
@@ -17,21 +17,24 @@ SSM_PATH = config["ssm_path"]
 
 
 def _write_sheet_to_s3(sheet_id_variable: str, dataset_name: str):
-    # Read data from Google Sheet
-    df = pd.DataFrame(get_data_from_gsheet(Variable.get(sheet_id_variable), SSM_PATH))
+
+    sheet_data = get_data_from_gsheet(
+        Variable.get(sheet_id_variable), SSM_PATH
+    )
+    df = pd.DataFrame(sheet_data)
 
     if df.empty:
         logger.info(f"No data found in {dataset_name}. Skipping S3 write.")
         return
 
-    # Remove duplicates just in case
     df = df.drop_duplicates()
 
-    # Generate timestamped file path
     current_date = get_current_datetime()
-    s3_path = f"s3://{BUCKET_NAME}/{FOLDER_NAME}/raw/{dataset_name}/{current_date}.parquet"
+    s3_path = (
+        f"s3://{BUCKET_NAME}/{FOLDER_NAME}/raw/"
+        f"{dataset_name}/{current_date}.parquet"
+    )
 
-    # Write to S3
     wr.s3.to_parquet(df=df, path=s3_path, dataset=False)
     logger.info(f"{dataset_name} data written to S3 at {s3_path}")
 
