544d29dcc3f2269e5a0c221ba15a023ecc4861f1
Jhoie <victoriousjvictor@gmail.com>
2026-04-09 10:32:32 +0100

    Add logging

--- business_logic/liffey_luxury/gsheets_to_s3_extract.py
+++ business_logic/liffey_luxury/gsheets_to_s3_extract.py
@@ -2,8 +2,12 @@ import os
 from dotenv import load_dotenv                                                                           
 load_dotenv()
 
+import logging
+
 import awswrangler as wr
 import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 from sqlalchemy import create_engine
 from plugins.date_utils import get_current_datetime
@@ -31,29 +35,49 @@ def gsheet_to_s3(gsheet_id: str, s3_path: str):
     """
 
     data = get_data_from_gsheet(gsheet_id, SSM_PATH)
+    logger.info(f"Data extracted from Google Sheet with ID: {gsheet_id}")
+
     df_marketing = pd.DataFrame(data)
 
+    if df_marketing.empty:
+        logger.warning("No data found in the Google Sheet.")
+        raise ValueError("No data to write to S3.")
+
     wr.s3.to_parquet(df_marketing, s3_path)
+    logger.info(f"Data written to S3: {s3_path}")
 
 gsheet_to_s3(GSHEET_ID, MARKETING_S3_PATH)
 
 
-def postgres_to_s3():
+# PostgreSQL extraction
+query = """
+    SELECT *
+    FROM historical.liffey_luxury_order_transactions
+    LIMIT 100;
+"""
+
+def postgres_to_s3(url: str, query: str, s3_path: str):
     """Extract data from a PostgreSQL database and write to S3 in Parquet format.
     """
-    db_url = os.getenv("DATABASE_URL")
+
+    logger.info("Starting PostgreSQL to S3 extraction.")
+
+    db_url = os.getenv(url)
     if not db_url:
-        raise ValueError("DATABASE_URL is missing")
+        logger.error("Database environment variable is not set.")
+        raise ValueError("Database environment variable is missing")
     
+    logger.info("Connecting to PostgreSQL database.")
     engine = create_engine(db_url)
 
-    query = """
-        SELECT *
-        FROM historical.liffey_luxury_order_transactions
-        LIMIT 100;
-    """
-
     df_orders = pd.read_sql(query, engine)
-    wr.s3.to_parquet(df_orders, ORDERS_S3_PATH)
+    logger.info(f"Data extracted from PostgreSQL: {df_orders.shape[0]} rows")
 
-postgres_to_s3()
+    if df_orders.empty:
+        logger.warning("No data found in the PostgreSQL query.")
+        raise ValueError("No data to write to S3.")
+
+    wr.s3.to_parquet(df_orders, s3_path)
+    logger.info(f"Data written to S3: {s3_path}")
+
+postgres_to_s3("DATABASE_URL", query, ORDERS_S3_PATH)
