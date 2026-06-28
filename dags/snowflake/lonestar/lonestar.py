10581ef6497ccbfb7a73c8063c481dee839a70b7
unknown <ojokayode13@gmail.com>
2026-06-29 00:09:32 +0100

    Created a file for sql and added airflow exception to lonestar dag

--- dags/snowflake/lonestar/lonestar.py
+++ dags/snowflake/lonestar/lonestar.py
@@ -3,26 +3,14 @@ from datetime import timedelta
 
 import pendulum
 from airflow import DAG
+from airflow.exceptions import AirflowException
 from airflow.models import Variable
 from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
-logger = logging.getLogger(__name__)
+from dags.snowflake.lonestar.copy_into_bronze import COPY_INTO_BRONZE
 
-COPY_INTO_BRONZE = """
-COPY INTO LONESTAR_PROD_DB.BRONZE.LONESTAR_DAILY_LOAD
-    (LONESTAR_DATA, SOURCE_FILE_NAME, FILE_ROW_NUMBER, FILE_LAST_MODIFIED)
-FROM (
-    SELECT
-        $1,
-        METADATA$FILENAME,
-        METADATA$FILE_ROW_NUMBER,
-        METADATA$FILE_LAST_MODIFIED
-    FROM @LONESTAR_PROD_DB.BRONZE.BRONZE_S3_STAGE
-)
-FILE_FORMAT = (FORMAT_NAME = 'LONESTAR_PROD_DB.BRONZE.JSON_S3_DAILY')
-ON_ERROR = 'ABORT_STATEMENT';
-"""
+logger = logging.getLogger(__name__)
 
 default_args = {
     "owner": "Federated-Engineers",
@@ -60,7 +48,7 @@ with DAG(
             deserialize_json=True
         )
 
-    except KeyError as exc:
+    except (KeyError, AirflowException) as exc:
         logger.error(
             "Airflow variable 'elite_lonestar_dbt_network' not found"
         )
