1170a4dac805219b357aa35cb5011f163dd1d76c
taofeecoh <adesanutaofeecoh@gmail.com>
2026-08-23 13:31:41 +0100

    modify folder name

---
+++ business_logic/august_client_nordic_peaks/pipeline.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.path.append("/home/taofeecoh/federated/nordic/elite-airflow")
+
+from module import load_gsheet_to_s3
+
+from config import (DATA_SOURCES, S3_FOLDER_PATH,
+                    SERVICE_ACCOUNT_CREDENTIALS_PATH)
+
+data_sources_key = [(source_name, id) for source_name, id in DATA_SOURCES.items()]
+
+load = load_gsheet_to_s3(
+    googlesheet_id=data_sources_key[0][1],
+    ssm_path=SERVICE_ACCOUNT_CREDENTIALS_PATH,
+    s3_folder_path=S3_FOLDER_PATH,
+    file_name=data_sources_key[0][0]
+)
