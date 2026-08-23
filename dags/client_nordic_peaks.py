73c95da9d8b321e7a8823e6d3ad1f9e913c141a7
taofeecoh <adesanutaofeecoh@gmail.com>
2026-08-23 15:12:45 +0100

    quality check on pipeline

--- dags/client_nordic_peaks.py
+++ dags/client_nordic_peaks.py
@@ -1,10 +1,8 @@
-import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
-
 from business_logic.august_client_nordic_peaks.config import (
     DATA_SOURCES, S3_FOLDER_PATH, SERVICE_ACCOUNT_CREDENTIALS_PATH)
 from business_logic.august_client_nordic_peaks.module import load_gsheet_to_s3
@@ -25,22 +23,20 @@ with DAG(
 
     start_tasks = EmptyOperator(task_id="start")
 
-
     extract_sheets = [
-          PythonOperator(
-          task_id=f"extract_{sheet_name}",
-          python_callable=load_gsheet_to_s3,
-          op_kwargs={
-                "googlesheet_id": DATA_SOURCES[sheet_id],
+        PythonOperator(
+            task_id=f"extract_{sheet_name}",
+            python_callable=load_gsheet_to_s3,
+            op_kwargs={
+                "googlesheet_id": sheet_id,
                 "ssm_path": SERVICE_ACCOUNT_CREDENTIALS_PATH,
                 "folder_path": S3_FOLDER_PATH,
-                "file_name": DATA_SOURCES[sheet_name]
-          }
+                "file_name": sheet_name
+            }
         )
 
-          for sheet_name, sheet_id in DATA_SOURCES.items()
+        for sheet_name, sheet_id in DATA_SOURCES.items()
     ]
-
 
     end_tasks = EmptyOperator(task_id="end")
 
