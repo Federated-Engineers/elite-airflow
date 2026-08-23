6ff6e5a7614055ae73a0cfe75952c115ecc941f6
taofeecoh <adesanutaofeecoh@gmail.com>
2026-08-23 20:25:02 +0100

    fix imports

--- business_logic/august_client_nordic_peaks/pipeline.py
+++ business_logic/august_client_nordic_peaks/pipeline.py
@@ -5,14 +5,13 @@ from config import (
 )
 from module import load_gsheet_to_s3
 
-data_sources_key = ([(source_name, id)
-                     for source_name, id in DATA_SOURCES.items()])
+data_sources_key = [(source_name, id) for source_name, id in DATA_SOURCES.items()]
 
 load_data = load_gsheet_to_s3(
     googlesheet_id=data_sources_key[0][1],
     ssm_path=SERVICE_ACCOUNT_CREDENTIALS_PATH,
     folder_path=S3_FOLDER_PATH,
-    file_name=data_sources_key[0][0]
+    file_name=data_sources_key[0][0],
 )
 
 print(load_data)
