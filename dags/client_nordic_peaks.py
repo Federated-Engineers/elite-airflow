761e21eb04940e62d0405ea67bc691d540b7ba54
taofeecoh <adesanutaofeecoh@gmail.com>
2026-08-23 15:29:41 +0100

    modify pipeline

--- dags/client_nordic_peaks.py
+++ dags/client_nordic_peaks.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
+
 from business_logic.august_client_nordic_peaks.config import (
     DATA_SOURCES, S3_FOLDER_PATH, SERVICE_ACCOUNT_CREDENTIALS_PATH)
 from business_logic.august_client_nordic_peaks.module import load_gsheet_to_s3
