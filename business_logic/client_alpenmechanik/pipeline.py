a951a0a41ad85d30d78dc7493d1c3e46785c29f7
taofeecoh <adesanutaofeecoh@gmail.com>
2026-09-10 22:24:23 +0100

    add business logic

--- business_logic/client_alpenmechanik/pipeline.py
+++ business_logic/client_alpenmechanik/pipeline.py
@@ -1,6 +1,7 @@
+from module import load_gsheet_to_s3
+
 from config import (DATA_SOURCE, S3_FOLDER_PATH,
                     SERVICE_ACCOUNT_CREDENTIALS_PATH)
-from module import load_gsheet_to_s3
 
 load_data = load_gsheet_to_s3(
     googlesheet_id=DATA_SOURCE,
