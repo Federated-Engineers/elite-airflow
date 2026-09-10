1a8468add7b30baf659c9c0ee7214b34a911dedb
taofeecoh <adesanutaofeecoh@gmail.com>
2026-09-10 20:29:36 +0100

    add config file

--- business_logic/client_alpenmechanik/config.py
+++ business_logic/client_alpenmechanik/config.py
@@ -5,8 +5,8 @@ DATA_SOURCE = "1IIr3cYvnT7T7IWMD-naJ-IqghvOgP5aFEybT-7ecO2w"
 SERVICE_ACCOUNT_CREDENTIALS_PATH = (
     "/production/google-service-account/credentials"
     )
-BUCKET_NAME = "federated-engineers-staging-elite-data-lake"
-# "federated-engineers-production-elite-client-alpenmechanik"
+# BUCKET_NAME = "federated-engineers-staging-elite-data-lake"
+BUCKET_NAME = "federated-engineers-production-elite-client-alpenmechanik"
 
 S3_FOLDER_PATH = s3_full_path(
     BUCKET_NAME, "repairpartner"
